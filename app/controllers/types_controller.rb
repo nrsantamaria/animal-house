fbbd352d331d9e01e5c64f754d929bcf259c95e2
Nicole Santamaria <nicolersantamaria@gmail.com>
2017-07-21 09:51:21 -0700

    add create to type controller

--- app/controllers/types_controller.rb
+++ app/controllers/types_controller.rb
@@ -8,4 +8,14 @@ class TypesController < ApplicationController
     @type = Type.find(params[:id])
     json_response(@type)
   end
+
+  def create
+    @type = Type.create!(type_params)
+    json_response(@type, :created)
+  end
+
+  private
+  def type_params
+    params.permit(:species)
+  end
 end
