76f166b4d4746711a8be8e8f8d273d082e789809
Nicole Santamaria <nicolersantamaria@gmail.com>
2017-07-21 16:09:16 -0700

    add validations to animal model. pass spec.

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,4 +1,5 @@
 class Animal < ApplicationRecord
   belongs_to :type
+  validates :name, :color, :breed, :age, :gender, :weight, :description, :photo, :presence => true
   paginates_per 5
 end
--- spec/models/animal_spec.rb
+++ spec/models/animal_spec.rb
@@ -2,4 +2,12 @@ require 'rails_helper'
 
 describe Animal do
   it { should belong_to :type }
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :color }
+  it { should validate_presence_of :breed }
+  it { should validate_presence_of :age }
+  it { should validate_presence_of :gender }
+  it { should validate_presence_of :weight }
+  it { should validate_presence_of :description }
+  it { should validate_presence_of :photo }
 end
