cd9e0e593031feec92f35b2e74cded7afe637fd9
Nicole Santamaria <nicolersantamaria@gmail.com>
2017-07-21 09:58:07 -0700

    add validation of species to type model. spec pass.

--- app/models/type.rb
+++ app/models/type.rb
@@ -1,3 +1,4 @@
 class Type < ApplicationRecord
   has_many :animals
+  validates :species, :presence => true
 end
--- spec/models/type_spec.rb
+++ spec/models/type_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 describe Type do
   it { should have_many :animals }
+  it { should validate_presence_of :species }
 end
